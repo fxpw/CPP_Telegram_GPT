666901c1473b44df86f631bb419c3e5ecc5cabcc
fxpw <fxpw@outlook.com>
2024-02-24 16:48:33 +0300

    fix lang

--- src/custom_gpt_chat.cpp
+++ src/custom_gpt_chat.cpp
@@ -7,26 +7,28 @@ CustomGPTChat::CustomGPTChat(std::string openai_api_key, std::string openai_mode
 
 std::string CustomGPTChat::MakeRequest(std::string request_text)
 {
-	nlohmann::json request;
-	request["model"] = model;
-	request["messages"] = nlohmann::json::array({ 
-		{{"role", "user"}, {"content", request_text}} 
-	});
-	int max_tokens = 4096 - static_cast<int>(request_text.size());
-	request["max_tokens"] = max_tokens > 0 ? max_tokens : 0;
-	request["temperature"] = 0;
-	auto response = this->openai_instance->chat.create(request);
-	// fmt::print(std::string(response.dump(4)));
-	std::cout<<response.dump(4)<<std::endl;
-    if (response.contains("choices") && response["choices"].is_array()) {
-        auto choices = response["choices"];
-        if (!choices.empty() && choices[0].contains("message") && choices[0]["message"].contains("content")) {
-            std::string content = choices[0]["message"]["content"];
-			// fmt::print(fmt::format("{}",std::string(response.dump(4))));
-            return content;
-        }
-    }
-	// for debug
-	return response.dump(4);
+	try {
+		nlohmann::json request;
+		request["model"] = model;
+		int max_tokens = 4096 - static_cast<int>(request_text.size());
+		std::string truncated_text = request_text.substr(0, 4095);
+		request["messages"] = nlohmann::json::array({ 
+			{{"role", "user"}, {"content", truncated_text}} 
+		});
+		request["max_tokens"] = max_tokens > 0 ? max_tokens : 0;
+		request["temperature"] = 0;
+		auto response = this->openai_instance->chat.create(request);
+		std::cout<<response.dump(4)<<std::endl;
+		if (response.contains("choices") && response["choices"].is_array()) {
+			auto choices = response["choices"];
+			if (!choices.empty() && choices[0].contains("message") && choices[0]["message"].contains("content")) {
+				std::string content = choices[0]["message"]["content"];
+				return content;
+			}
+		}
+		return response.dump(4);
+	}catch (const std::exception& e) {
+		return fmt::format("Error: {}", e.what());
+	}
 }
 
